72f773376cfbd1f6f7d55ae6e0283d1ac978fc54
Shatko <mladen.salaj@gmail.com>
2025-02-08 15:22:34 +0100

    feat: sets footer links active states

--- web/app/themes/interprojekt-studio/resources/views/sections/footer.blade.php
+++ web/app/themes/interprojekt-studio/resources/views/sections/footer.blade.php
@@ -8,23 +8,23 @@
         </td>
       </tr>
       <tr>
-        <td class="footer-cell" rowspan="1"><a href="{{ home_url('/') }}">Projektiranje</a></td>
+        <td class="footer-cell" rowspan="1"><a href="{{ home_url('/') }}" @class(['active' => is_front_page()])>Projektiranje</a></td>
         <td class="footer-cell-center" rowspan="1">gsm: 385 (0)97 797 47 80</td>
       </tr>
       <tr>
-        <td class="footer-cell" rowspan="1"><a href="{{ home_url('/o-nama') }}">O nama</a></td>
+        <td class="footer-cell" rowspan="1"><a href="{{ home_url('/o-nama') }}" @class(['active' => is_page('o-nama')])>O nama</a></td>
         <td class="footer-cell-center" rowspan="1">mihaela@interprojekt-studio.com</td>
       </tr>
       <tr>
-        <td class="footer-cell" rowspan="1"><a href="{{ home_url('/galerija') }}">Galerija</a></td>
+        <td class="footer-cell" rowspan="1"><a href="{{ home_url('/galerija') }}" @class(['active' => is_page('galerija')])>Galerija</a></td>
         <td class="footer-cell-center" rowspan="1">datum: @php echo date("d.m.Y"); @endphp</td>
       </tr>
       <tr>
-        <td class="footer-cell" rowspan="1"><a href="{{ home_url('/partneri') }}">Partneri</a></td>
+        <td class="footer-cell" rowspan="1"><a href="{{ home_url('/partneri') }}" @class(['active' => is_page('partneri')])>Partneri</a></td>
         <td class="footer-cell-center" colspan="2" rowspan="2">Trenutna Lokacija: <h4>{!! $title !!}</h4></td>
       </tr>
       <tr>
-        <td class="footer-cell" rowspan="1"><a href="{{ home_url('/kontakt') }}">Kontakt</a></td>
+        <td class="footer-cell" rowspan="1"><a href="{{ home_url('/kontakt') }}" @class(['active' => is_page('kontakt')])>Kontakt</a></td>
       </tr>
     </tbody>
   </table>
