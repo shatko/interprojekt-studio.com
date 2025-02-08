efacb40f0315caa0b96abf0984a0ab8079de8d81
Shatko <mladen.salaj@gmail.com>
2025-02-08 15:02:36 +0100

    feat: adds links to footer navigation

--- web/app/themes/interprojekt-studio/resources/views/sections/footer.blade.php
+++ web/app/themes/interprojekt-studio/resources/views/sections/footer.blade.php
@@ -8,23 +8,23 @@
         </td>
       </tr>
       <tr>
-        <td class="footer-cell" rowspan="1">Projektiranje</td>
+        <td class="footer-cell" rowspan="1"><a href="{{ home_url('/') }}">Projektiranje</a></td>
         <td class="footer-cell-center" rowspan="1">gsm: 385 (0)97 797 47 80</td>
       </tr>
       <tr>
-        <td class="footer-cell" rowspan="1">O nama</td>
+        <td class="footer-cell" rowspan="1"><a href="{{ home_url('/o-nama') }}">O nama</a></td>
         <td class="footer-cell-center" rowspan="1">mihaela@interprojekt-studio.com</td>
       </tr>
       <tr>
-        <td class="footer-cell" rowspan="1">Galerija</td>
+        <td class="footer-cell" rowspan="1"><a href="{{ home_url('/galerija') }}">Galerija</a></td>
         <td class="footer-cell-center" rowspan="1">datum: @php echo date("d.m.Y"); @endphp</td>
       </tr>
       <tr>
-        <td class="footer-cell" rowspan="1">Partneri</td>
+        <td class="footer-cell" rowspan="1"><a href="{{ home_url('/partneri') }}">Partneri</a></td>
         <td class="footer-cell-center" colspan="2" rowspan="2">Trenutna Lokacija: <h4>{!! $title !!}</h4></td>
       </tr>
       <tr>
-        <td class="footer-cell" rowspan="1">Kontakt</td>
+        <td class="footer-cell" rowspan="1"><a href="{{ home_url('/kontakt') }}">Kontakt</a></td>
       </tr>
     </tbody>
   </table>
