1a1c997d550ae54201f9b76c4fe90f3b33b4a5af
Shatko <mladen.salaj@gmail.com>
2025-02-08 14:56:49 +0100

    fix: replaces footer styling from inline

--- web/app/themes/interprojekt-studio/resources/views/sections/footer.blade.php
+++ web/app/themes/interprojekt-studio/resources/views/sections/footer.blade.php
@@ -1,31 +1,30 @@
-<footer class="content-info flex">
-  <table class="ml-auto">
+<footer class="content-info">
+  <table class="footer-table">
     <tbody>
       <tr>
-        <td class="p-2 border-[2px] border-solid border-black text-center" colspan="2">
-          <img class="w-64 inline" src="@asset('images/logo-hollow.png')" alt="{!! $siteName !!}">
-          <p class="text-[12px]">International Project Studio d.o.o., Celjska 39, 10 000 Zagreb</p>
+        <td class="footer-logo-cell" colspan="2">
+          <img class="footer-logo" src="@asset('images/logo-hollow.png')" alt="{!! $siteName !!}">
+          <p class="footer-address">International Project Studio d.o.o., Celjska 39, 10 000 Zagreb</p>
         </td>
       </tr>
       <tr>
-        <td class="text-sm p-1 border-[2px] border-solid border-black" rowspan="1">Projektiranje</td>
-        <td class="text-sm text-center p-1 border-[2px] border-solid border-black" rowspan="1">gsm: 385 (0)97 797 47 80</td>
+        <td class="footer-cell" rowspan="1">Projektiranje</td>
+        <td class="footer-cell-center" rowspan="1">gsm: 385 (0)97 797 47 80</td>
       </tr>
       <tr>
-        <td class="text-sm p-1 border-[2px] border-solid border-black" rowspan="1">O nama</td>
-        <td class="text-sm text-center p-1 border-[2px] border-solid border-black" rowspan="1">mihaela@interprojekt-studio.com</td>
+        <td class="footer-cell" rowspan="1">O nama</td>
+        <td class="footer-cell-center" rowspan="1">mihaela@interprojekt-studio.com</td>
       </tr>
       <tr>
-        <td class="text-sm p-1 border-[2px] border-solid border-black" rowspan="1">Galerija</td>
-        <td class="text-sm text-center p-1 border-[2px] border-solid border-black" rowspan="1">datum: @php echo date("d.m.Y"); @endphp</td>
+        <td class="footer-cell" rowspan="1">Galerija</td>
+        <td class="footer-cell-center" rowspan="1">datum: @php echo date("d.m.Y"); @endphp</td>
       </tr>
       <tr>
-        <td class="text-sm p-1 border-[2px] border-solid border-black" rowspan="1">Partneri</td>
-        <td class="text-sm text-center p-1 border-[2px] border-solid border-black" rowspan="1">Trenutna Lokacija:</td>
+        <td class="footer-cell" rowspan="1">Partneri</td>
+        <td class="footer-cell-center" colspan="2" rowspan="2">Trenutna Lokacija: <h4>{!! $title !!}</h4></td>
       </tr>
       <tr>
-        <td class="text-sm p-1 border-[2px] border-solid border-black" rowspan="1">Kontakt</td>
-        <td class="text-sm text-center p-1 border-[2px] border-solid border-black" rowspan="1"><h4>{!! $title !!}</h4></td>
+        <td class="footer-cell" rowspan="1">Kontakt</td>
       </tr>
     </tbody>
   </table>
