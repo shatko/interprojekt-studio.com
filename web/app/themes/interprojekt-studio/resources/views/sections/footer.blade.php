e71977546cd635de36af813eaa28e0832b446cec
Shatko <mladen.salaj@gmail.com>
2025-02-08 12:53:40 +0100

    feat: sets basic footer

--- web/app/themes/interprojekt-studio/resources/views/sections/footer.blade.php
+++ web/app/themes/interprojekt-studio/resources/views/sections/footer.blade.php
@@ -1,3 +1,33 @@
-<footer class="content-info">
+<footer class="content-info flex">
+  <table class="ml-auto">
+    <tbody>
+      <tr>
+        <td class="p-2 border-[2px] border-solid border-black text-center" colspan="2">
+          <img class="w-64 inline" src="@asset('images/logo-hollow.png')" alt="{!! $siteName !!}">
+          <p class="text-[12px]">International Project Studio d.o.o., Celjska 39, 10 000 Zagreb</p>
+        </td>
+      </tr>
+      <tr>
+        <td class="text-sm p-1 border-[2px] border-solid border-black" rowspan="1">Projektiranje</td>
+        <td class="text-sm text-center p-1 border-[2px] border-solid border-black" rowspan="1">gsm: 385 (0)97 797 47 80</td>
+      </tr>
+      <tr>
+        <td class="text-sm p-1 border-[2px] border-solid border-black" rowspan="1">O nama</td>
+        <td class="text-sm text-center p-1 border-[2px] border-solid border-black" rowspan="1">mihaela@interprojekt-studio.com</td>
+      </tr>
+      <tr>
+        <td class="text-sm p-1 border-[2px] border-solid border-black" rowspan="1">Galerija</td>
+        <td class="text-sm text-center p-1 border-[2px] border-solid border-black" rowspan="1">datum: @php echo date("d.m.Y"); @endphp</td>
+      </tr>
+      <tr>
+        <td class="text-sm p-1 border-[2px] border-solid border-black" rowspan="1">Partneri</td>
+        <td class="text-sm text-center p-1 border-[2px] border-solid border-black" rowspan="1">Trenutna Lokacija:</td>
+      </tr>
+      <tr>
+        <td class="text-sm p-1 border-[2px] border-solid border-black" rowspan="1">Kontakt</td>
+        <td class="text-sm text-center p-1 border-[2px] border-solid border-black" rowspan="1"><h1>{!! $title !!}</h1></td>
+      </tr>
+    </tbody>
+  </table>
 
 </footer>
--- web/app/themes/interprojekt-studio/resources/views/sections/header.blade.php
+++ web/app/themes/interprojekt-studio/resources/views/sections/header.blade.php
@@ -1,13 +1,7 @@
 <header class="banner mt-4 flex">
   <a class="brand w-48 mr-auto" href="{{ home_url('/') }}">
-    <img class="" src="@asset('images/logo.png')" alt="{!! $siteName !!}">
+    <img src="@asset('images/logo.png')" alt="{!! $siteName !!}">
   </a>
 
   @include('partials.page-header')
-
-  @if (has_nav_menu('primary_navigation'))
-    <nav class="nav-primary" aria-label="{{ wp_get_nav_menu_name('primary_navigation') }}">
-      {!! wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav', 'echo' => false]) !!}
-    </nav>
-  @endif
 </header>
