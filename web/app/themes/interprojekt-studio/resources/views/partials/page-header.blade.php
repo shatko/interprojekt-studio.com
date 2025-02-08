304e741818405fba189810804ded3124cf7e5060
Shatko <mladen.salaj@gmail.com>
2025-02-08 09:45:30 +0100

    feat: sets header

--- web/app/themes/interprojekt-studio/resources/views/partials/page-header.blade.php
+++ web/app/themes/interprojekt-studio/resources/views/partials/page-header.blade.php
@@ -1,3 +1,7 @@
-<div class="page-header">
-  <h1>{!! $title !!}</h1>
+<div class="flex flex-col">
+  <div class="page-header ml-auto">
+    <h1>{!! $title !!}</h1>
+  </div>
+
+  @svg('images/svg/compass', 'w-24 h-auto ml-auto')
 </div>
--- web/app/themes/interprojekt-studio/resources/views/sections/header.blade.php
+++ web/app/themes/interprojekt-studio/resources/views/sections/header.blade.php
@@ -1,7 +1,9 @@
-<header class="banner">
-  <a class="brand" href="{{ home_url('/') }}">
-    {!! $siteName !!}
+<header class="banner mt-4 flex">
+  <a class="brand w-48 mr-auto" href="{{ home_url('/') }}">
+    <img class="" src="@asset('images/logo.png')" alt="{!! $siteName !!}">
   </a>
+
+  @include('partials.page-header')
 
   @if (has_nav_menu('primary_navigation'))
     <nav class="nav-primary" aria-label="{{ wp_get_nav_menu_name('primary_navigation') }}">
